4db9de45e30d3acec7307f602615691416981218
Andre Mello <amellow@Andres-MacBook-Air.local>
2023-05-19 21:50:00 -0400

    global state

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { createRoot } from "react-dom/client";
 import SearchParams from "./searchParams";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -5,6 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Details from "./Details";
 import { Link } from "react-router-dom";
 import React from "react";
+import AdoptedPetContext from './adoptedPetContext';
 
 const queryClient = new QueryClient({
   defaultOptoins: {
@@ -16,17 +18,19 @@ const queryClient = new QueryClient({
 });
 
 const App = () => {
+  const adoptedPet = useState(null)
   return (
     <div>
       <BrowserRouter>
         <QueryClientProvider client={queryClient}>
+          <AdoptedPetContext.Provider value={adoptedPet}>
           <header>
             <Link to="/">Adopt Me!</Link>
           </header>
           <Routes>
             <Route path="/details/:id" element={<Details />} />
             <Route path="/" element={<SearchParams />} />
-          </Routes>
+          </Routes></AdoptedPetContext.Provider>
         </QueryClientProvider>
       </BrowserRouter>
     </div>
